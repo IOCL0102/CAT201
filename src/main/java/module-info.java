295be89f6752fe902a52316c8eb0d3e0683ec953
thamsiewteng <siewtengtham@gmail.com>
2022-01-23 17:48:34 +0800

    Added movie info and home page

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.example.cat201_project {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires json.simple;
+
+    opens com.example.cat201_project to javafx.fxml;
+    exports com.example.cat201_project;
+}

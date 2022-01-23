c7371cfc1dd880c8e717ac5c42a2896366963f02
thamsiewteng <siewtengtham@gmail.com>
2022-01-23 17:43:28 +0800

    Added home movie page

---
+++ src/main/java/com/example/cat201_project/HomeMovie.java
@@ -0,0 +1,27 @@
+package com.example.cat201_project;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HomeMovie extends Application
+{
+    @Override
+    public void start(Stage stage) throws IOException
+    {
+        FXMLLoader fxmlLoader = new FXMLLoader(com.example.cat201_project.HomeMovie.class.getResource("home-movie.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
+        scene.getStylesheets().add(getClass().getResource("HomeMovie.css").toExternalForm());
+        stage.setTitle("ROYAL CINEMA!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args)
+    {
+        launch();
+    }
+}

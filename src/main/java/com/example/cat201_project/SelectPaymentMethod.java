73b361a904e7ca0f52c3271714d797be482772ba
ZH <chongzhanhang2000@gmail.com>
2022-01-25 20:57:17 +0800

    Drafted basic functions for main payment page and controller class for main payment page

---
+++ src/main/java/com/example/cat201_project/SelectPaymentMethod.java
@@ -0,0 +1,27 @@
+package com.example.cat201_project;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.*;
+//import java.lang.reflect.InvocationTargetException;
+
+public class SelectPaymentMethod extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException{
+        FXMLLoader fxmlLoader = new FXMLLoader(SelectPaymentMethod.class.getResource("selectpayment.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
+        scene.getStylesheets().add(getClass().getResource("selectpayment.css").toExternalForm());
+        stage.setTitle("ROYAL CINEMA!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args){
+
+        launch();
+    }
+}
